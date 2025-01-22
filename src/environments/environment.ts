8364bc5a23c64cbecad9f8157d8f7555a2df5c8b
Kalli <kalli_28456@hotmail.com>
2025-01-22 01:08:19 -0500

    entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  webApiUrl:'http://127.0.0.1:8005',
-  // webApiUrl:'http://9.169.191.93',
+  // webApiUrl:'http://127.0.0.1:8005',
+  webApiUrl:'http://9.169.191.93',
   // webApiUrl:'https://539r153q-8005.brs.devtunnels.ms',
   production: false
 };
